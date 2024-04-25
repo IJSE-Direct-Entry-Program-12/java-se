7cf20543197328c205a030a56aedee0d3785b4c1
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2024-04-25 16:26:32 +0530

    Add dep12-general-chat-app-server

---
+++ io/networking/dep12-general-chat-app/server-app/src/main/java/lk/ijse/dep12/server/ServerAppInitializer.java
@@ -0,0 +1,60 @@
+package lk.ijse.dep12.server;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+public class ServerAppInitializer {
+
+    private static final List<Socket> CLIENT_LIST = new CopyOnWriteArrayList<>();
+
+    public static void main(String[] args) throws IOException {
+        try (ServerSocket serverSocket = new ServerSocket(5050)) {
+            while (true) {
+                Socket localSocket = serverSocket.accept();
+                CLIENT_LIST.add(localSocket);
+                broadcastMessage(localSocket, "Entered: " + localSocket.getRemoteSocketAddress());
+
+                new Thread(() -> {
+                    try {
+                        InputStream is = localSocket.getInputStream();
+                        byte[] byteBuffer = new byte[1024];
+                        while (true) {
+                            int read = is.read(byteBuffer);
+                            if (read == -1) {
+                                broadcastMessage(localSocket, "Exited: " + localSocket.getRemoteSocketAddress());
+                                CLIENT_LIST.remove(localSocket);
+                                break;
+                            }
+                            String message = new String(byteBuffer, 0, read);
+                            broadcastMessage(localSocket, message);
+                            Arrays.fill(byteBuffer, (byte) 0);
+                        }
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                }).start();
+            }
+        }
+    }
+
+    private static void broadcastMessage(Socket client, String message) {
+        new Thread(() -> {
+            for (Socket socket : CLIENT_LIST) {
+                if (socket != client) continue;
+
+                try {
+                    if (socket.isConnected()) {
+                        socket.getOutputStream().write(message.getBytes());
+                    }
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }).start();
+    }
+}
