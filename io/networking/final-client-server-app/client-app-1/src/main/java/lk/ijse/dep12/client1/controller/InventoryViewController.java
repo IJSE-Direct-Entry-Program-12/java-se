0c554278b1cbd8d7c3e41b74e7017cad6e77349f
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2024-04-26 11:41:58 +0530

    Finialize the validation

--- io/networking/final-client-server-app/client-app-1/src/main/java/lk/ijse/dep12/client1/controller/InventoryViewController.java
+++ io/networking/final-client-server-app/client-app-1/src/main/java/lk/ijse/dep12/client1/controller/InventoryViewController.java
@@ -7,6 +7,8 @@ import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.input.KeyEvent;
 
+import java.math.BigDecimal;
+
 public class InventoryViewController {
 
     public Button btnDelete;
@@ -27,7 +29,42 @@ public class InventoryViewController {
     }
 
     public void btnSaveOnAction(ActionEvent event) {
+        TextField invalidField;
+        if ((invalidField = validateData()) != null) {
+            invalidField.requestFocus();
+            invalidField.selectAll();
+            return;
+        }
+    }
 
+    private TextField validateData(){
+        String barcode = txtBarcode.getText();
+        String description = txtDescription.getText();
+        String qty = txtQty.getText();
+        String price = txtPrice.getText();
+
+        if (barcode.isBlank() || !isNumber(barcode.strip())) return txtBarcode;
+        else if (description.isBlank() || description.length() < 3) return txtDescription;
+        else if (qty.isBlank() || !isNumber(qty) || Integer.parseInt(qty) <= 0) return txtQty;
+        else if (price.isBlank() || !isPrice(price)) return txtPrice;
+
+        return null;
+    }
+
+    private boolean isPrice(String input){
+        try {
+            BigDecimal price = new BigDecimal(input);
+            return price.compareTo(BigDecimal.ZERO) > 0;
+        }catch (NumberFormatException e){
+            return false;
+        }
+    }
+
+    private boolean isNumber(String input){
+        for (char c : input.toCharArray()) {
+            if (!Character.isDigit(c)) return false;
+        }
+        return true;
     }
 
     public void tblInventoryOnKeyPressed(KeyEvent event) {
